f72a49960f25cf97695a981a84a891efc0a84b1d
chidiya <shriya.tees@gmail.com>
2023-07-20 00:34:06 -0700

    added some comments

--- App.js
+++ App.js
@@ -1,4 +1,7 @@
-import React from 'react';
+// Danica - D, Sneha - S, Shriya - C
+// Let's try to comment on the stuff we add so we can keep track of who changes what
+
+import React from 'react'; // added this
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View, Image, Button } from 'react-native';
 
@@ -6,14 +9,14 @@ export default function App() {
   return (
     <View style={styles.container}>
       <Image
-        source={require('./assets/templogo.png')}
+        source={require('./assets/templogo.png')} // temporary logo/image for start page - C
         style={styles.logo}
-        resizeMode="contain" 
+        resizeMode="contain" // so the png photo isn't a messed up size - C
       />
       <Text style={styles.title}>TaxXperts</Text>
       <Text style={styles.subtitle}>Building Financial Foundations for Young Adults</Text>
       <Button
-        title="CONTINUE"
+        title="CONTINUE" // I'm not sure we need a button? Maybe just have it continue to the next page on its own. I'll change it later once I figure out how - C
       />
       <StatusBar style="auto" />
     </View>
@@ -23,7 +26,7 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#e6e6fa',
+    backgroundColor: '#e6e6fa', // lavender-ish color, subject to change - C
     alignItems: 'center',
     justifyContent: 'center',
     padding: 16,
