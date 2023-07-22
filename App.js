bd250e3ae9dd5fc355eb080bc773eec6519358be
dt604121 <dt604@byui.edu>
2023-07-22 07:59:21 -0500

    Beware of TaxFraud

--- App.js
+++ App.js
@@ -2,12 +2,15 @@
 // Let's try to comment on the stuff we add so we can keep track of who changes what
 
 import React from 'react'; // added this'
+import { View } from 'react-native'; // added this
 import Firstscreen from './screens/Firstscreen';
+import BewareOfTaxFraud from './BewareOfTaxFraud'; // and this
 
 export default function App() {
   return (
     <View>
       <Firstscreen />
+      <BewareOfTaxFraud /> 
     </View>
   );
 }
---
+++ components/BewareOfTaxFraud.jsx
@@ -0,0 +1,67 @@
+import React from 'react';
+import { View, Text, Image, Linking, StyleSheet } from 'react-native';
+import { WebView } from 'react-native-webview';
+
+const BewareOfTaxFraud = () => {
+  return (
+    <View style={styles.container}>
+      <Text style={styles.heading}>Beware of Tax Fraud</Text>
+
+      <Text style={styles.description}>
+        Tax fraud is a serious issue that can have significant consequences. It's important to be aware of the signs of tax fraud and how to protect yourself. Here are some resources to help you stay informed:
+      </Text>
+
+      <Image source={require('./assets/taxfraud.jpg')} style={styles.image} />
+
+      <Text style={styles.subheading}>Videos:</Text>
+          <WebView source={{ uri: 'https://www.youtube.com/watch?v=ID_xGbn109I' }} style={styles.video} />
+          <WebView source={{ uri: 'https://www.youtube.com/watch?v=NglAffwNFho' }} style={styles.video} />
+
+      <Text style={styles.subheading}>Helpful Links:</Text>
+      <Text style={styles.link} onPress={() => Linking.openURL('https://www.irs.gov/individuals/how-do-you-report-suspected-tax-fraud-activity')}>IRS Identity Theft, Fraud & Scams</Text>
+        <Text style={styles.link} onPress={() => Linking.openURL('https://www.irs.gov/about-irs/irs-priorities-detecting-fraud-protecting-taxpayers')}>FTC Tax Identity Theft Awareness</Text>
+          <Text style={styles.link} onPress={() => Linking.openURL('https://www.irs.gov/about-irs/irs-priorities-detecting-fraud-protecting-taxpayers')}>FTC Tax Identity Theft Awareness</Text>
+          <Text style={styles.link} onPress={() => Linking.openURL('https://www.industriuscfo.com/seasons-new-online-tax-fraud-method/')}>FTC Tax Identity Theft Awareness</Text>
+
+    </View>
+  );
+};
+
+const styles = StyleSheet.create({
+  container: {
+    padding: 16,
+  },
+  heading: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#B8E2C8'
+  },
+  description: {
+    fontSize: 16,
+    marginBottom: 20,
+  },
+  image: {
+    width: '100%',
+    height: 200,
+    marginBottom: 20,
+  },
+  subheading: {
+    fontSize: 20,
+    fontWeight: 'bold',
+    marginBottom: 10,
+    color: '#B0A1BA',
+  },
+  video: {
+    width: '100%',
+    height: 200,
+    marginBottom: 20,
+  },
+  link: {
+    fontSize: 16,
+    color: '#A5B5BF',
+    marginBottom: 10,
+  },
+});
+
+export default BewareOfTaxFraud;
