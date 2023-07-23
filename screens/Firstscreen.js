b966137862c73acc3d26cdf994577621698c5677
chidiya <shriya.tees@gmail.com>
2023-07-23 00:11:46 -0700

    changes trying to debug and make screens work together

---
+++ screens/Firstscreen.js
@@ -0,0 +1,60 @@
+// I NEED TO FIX SOME STUFF IN THIS FILE, WILL COME BACK LATER
+
+import React from 'react'; // added this
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View, Image, Button } from 'react-native';
+import { useNavigation } from '@react-navigation/native';
+
+const Firstscreen = () => {
+  const navigation = useNavigation();
+  useEffect(() => {
+    const timer = setTimeout(() => {
+      navigation.navigate('BewareOfTaxFraud');
+    }, 3000);
+
+    return () => clearTimeout(timer);
+  }, [navigation]); 
+
+
+  return (
+    <View style={styles.container}>
+      <Image
+        source={require('../assets/templogo.png')} // temporary logo/image for start page - C
+        style={styles.logo}
+        resizeMode="contain" // so the png photo isn't a messed up size - C
+      />
+      <Text style={styles.title}>TaxXperts</Text>
+      <Text style={styles.subtitle}>Building Financial Foundations for Young Adults</Text>
+      <Button
+        title="CONTINUE" // I'm not sure we need a button? Maybe just have it continue to the next page on its own. I'll change it later once I figure out how - C
+      />
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#e6e6fa', // lavender-ish color, subject to change - C
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 16,
+  },
+  logo: {
+    width: 550,
+    height: 550,
+    marginBottom: 16,
+  },
+  title: {
+    fontSize: 24,
+    fontWeight: 'bold',
+    marginBottom: 16,
+  },
+  subtitle: {
+    fontSize: 16,
+    marginBottom: 32,
+  },
+});
+
+export default Firstscreen;
