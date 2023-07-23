95ceb97bcee8e663dbb72195b43e6b1004e96bbe
chidiya <shriya.tees@gmail.com>
2023-07-23 00:11:22 -0700

    trying to make the screens work together, not done yet, errors

--- App.js
+++ App.js
@@ -1,16 +1,26 @@
 // Danica - D, Sneha - S, Shriya - C
 // Let's try to comment on the stuff we add so we can keep track of who changes what
 
-import React from 'react'; // added this'
+import React, { useEffect } from 'react'; // added this'
 import { View } from 'react-native'; // added this
+
+
+import { NavigationContainer, useNavigation } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+
 import Firstscreen from './screens/Firstscreen';
-import BewareOfTaxFraud from './BewareOfTaxFraud'; // and this
+import BewareOfTaxFraud from './components/BewareOfTaxFraud'; // and this
+
+const stack = createStackNavigator();
 
 export default function App() {
   return (
-    <View>
-      <Firstscreen />
-      <BewareOfTaxFraud /> 
-    </View>
+    <NavigationContainer>
+      <stack.Navigator initialRouteName='Firstscreen'>
+        <stack.Screen name='Firstscreen' component={Firstscreen} />
+        <stack.Screen name='BewareOfTaxFraud' component={BewareOfTaxFraud} />
+      </stack.Navigator>
+    </NavigationContainer>
   );
 }
