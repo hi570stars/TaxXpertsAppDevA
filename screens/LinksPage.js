44759f98cd2da946ed5481dae0d9a33c1409b8b5
chidiya <shriya.tees@gmail.com>
2023-07-30 01:18:17 -0700

    working on links page, formatting (content to come)

--- screens/LinksPage.js
+++ screens/LinksPage.js
@@ -1,13 +1,21 @@
 import React from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, ImageBackground } from 'react-native';
+import Constants from 'expo-constants';
+
+const image = require('../assets/library.jpg');
 
 const LinksPage = () => {
   return (
+
+
     <View style={styles.container}>
-      <Text style={styles.heading}>Links Page</Text>
-      <Text style={styles.content}>
-        This is the Links Page. You can add your links or other content here.
-      </Text>
+      <ImageBackground source={image} style={styles.image}>
+        <View style={styles.header}>
+          <Text style={styles.headerText}>Find links below to various tax forms.</Text>
+        </View>
+        <Text style={styles.text}>Disclaimer: all links lead to government websites on taxes.</Text>
+        <Text style={styles.text}>TaxXperts is no liable for any misinformation or issues on linked websites. </Text>
+      </ImageBackground>
     </View>
   );
 };
@@ -15,17 +23,32 @@ const LinksPage = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#fff',
+    flexDirection: 'column',
+    justifyContent: 'flex-start', 
   },
-  heading: {
+  image: {
+    flex: 1,
+    resizeMode: 'cover',
+    justifyContent: 'center',
+  },
+  header: {
+    paddingTop: 20,
+  },
+  headerText: {
     fontSize: 24,
     fontWeight: 'bold',
-    marginBottom: 16,
+    textAlign: 'center',
+    color: 'white',
   },
-  content: {
-    fontSize: 18,
+  textContainer: {
+    flex: 1,
+    justifyContent: 'flex-start',
+    alignItems: 'center',
+    paddingTop: 20, 
+  },
+  text: {
+    color: 'white',
+    fontSize: 16,
     textAlign: 'center',
   },
 });
