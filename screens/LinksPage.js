14f2be3f1bda30b2462e8e0db6077e80fd5dab12
dt604121 <97199531+dt604121@users.noreply.github.com>
2023-08-10 18:03:36 -0500

    Update LinksPage.js w/info for boxes

--- screens/LinksPage.js
+++ screens/LinksPage.js
@@ -12,15 +12,15 @@ const LinksPage = ({ navigation }) => {
 
   const renderInfoButtons = () => {
     const buttonsData = [
-      { name: '1', text: 'According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground.' },
-      { name: '2', text: 'According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground.' },
-      { name: '3', text: 'According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground.' },
-      { name: '4', text: 'According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground.' },
-      { name: '5', text: 'According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground.' },
-      { name: '6', text: 'According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground.' },
-      { name: '7', text: 'According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground.' },
-      { name: '8', text: 'According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground.' },
-      { name: '9', text: 'According to all known laws of aviation, there is no way a bee should be able to fly. Its wings are too small to get its fat little body off the ground.' },
+      { name: '1', text: 'Answer: If you're under 18, you may be considered a dependent on your parents' or legal guardians' taxes. In such cases, you might not need to file your own tax return. Resource: If you're under 18, you might not need to file taxes. Learn more about dependent status: https://www.irs.gov/help/ita/whom-may-i-claim-as-a-dependent.' },
+      { name: '2', text: 'Answer: As a student, you should be aware of potential tax benefits related to education, such as the American Opportunity Credit and the Lifetime Learning Credit. Resource: Explore tax benefits for education, like the American Opportunity Credit:https://www.irs.gov/help/ita/am-i-eligible-to-claim-an-education-credit and Lifetime Learning Credit.' },
+      { name: '3', text: 'Answer: Everyone, regardless of occupation, should understand basic tax concepts, such as the difference between deductions and credits, and how to keep accurate records. Resource: Get acquainted with the basics of taxes, including deductions, credits, and record-keeping:https://apps.irs.gov/app/understandingTaxes/student/tax_tutorials.jsp' },
+      { name: '4', text: 'Sub-question: What is your income level? Answer: Your income level affects your tax liability. Higher income levels generally result in higher tax payments. Resource: Understand how tax brackets work and how they impact your tax payments:https://www.irs.gov/taxtopics/tc751.' },
+      { name: '5', text: 'Answer: If your gross income is above $12,950 (for single filers), you will likely need to file taxes. Make sure to report all sources of income accurately. Resource: Learn if you're required to file a federal tax return based on your income and situation:https://www.irs.gov/filing/individuals/who-should-file.' },
+      { name: '6', text: 'Answer: If your gross income is below $12,950 (for single filers), you might not be required to file taxes. However, it's still beneficial to file if you qualify for tax credits. Resource: Check the filing requirements and understand when filing is necessary, even with lower income:https://www.irs.gov/help/ita/do-i-need-to-file-a-tax-return.' },
+      { name: '7', text: 'Answer: Based on your responses, you may have a tax filing obligation. To understand your situation better, consult the official IRS guidelines:https://www.irs.gov/help/ita. Resource: Use the Interactive Tax Assistant to clarify your specific tax obligations based on your answers.' },
+      { name: '8', text: 'Answer: Managing student finances includes understanding student loan interest deductions and education-related tax credits. Resource: Delve into the IRS guide for students to comprehend federal income tax aspects related to education:chrome-extension://efaidnbmnnnibpcajpcglclefindmkaj/https://www.irs.gov/pub/irs-pdf/p970.pdf' },
+      { name: '9', text: 'Answer: To maximize your tax benefits, explore deductions like charitable contributions, medical expenses, and retirement contributions.Resource: Discover credits and deductions for individuals to make the most of potential tax savings:https://www.irs.gov/credits-deductions-for-individuals' },
     ];
 
     return buttonsData.map((button) => (
